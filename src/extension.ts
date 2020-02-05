03440bf579ee337521331f9a5e91b4ba28a10744
Nimo Beeren <nimo@deity.io>
2020-02-05 21:06:48 +0100

    support using any editor as input

--- src/extension.ts
+++ src/extension.ts
@@ -2,28 +2,44 @@ import * as vscode from "vscode";
 import { transform } from "./transform";
 
 const myScheme = "hide-typescript";
+const transformedDocumentSuffix = " (transformed)";
+
+const watchingDocuments = new Set<vscode.TextDocument>();
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.hideTypeScript", hideCommand)
+    vscode.commands.registerCommand("extension.hideTypeScript", hideTypeScript)
   );
 
   const TransformedCodeProvider = class
     implements vscode.TextDocumentContentProvider {
-    async provideTextDocumentContent(): Promise<string> {
-      // Get the source code from the active editor
-      const { activeTextEditor } = vscode.window;
-      if (!activeTextEditor) {
-        throw new Error("Lost the active editor");
+    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+      // Naively escape suffix string for use in regex
+      // This will break when suffix contains symbols reserved by regex other than ) and (
+      const escapedSuffix = transformedDocumentSuffix
+        .replace("(", "\\(")
+        .replace(")", "\\)");
+
+      const sourceDocumentName = uri.path.replace(
+        new RegExp(`${escapedSuffix}$`),
+        ""
+      );
+
+      // Find the editor that contains the source document
+      const sourceEditor = findEditorByDocumentName(sourceDocumentName);
+      if (!sourceEditor) {
+        throw new Error("Couldn't find the source editor");
       }
-      const input = activeTextEditor.document.getText();
+
+      // Get the source code from the editor
+      const sourceCode = sourceEditor.document.getText();
 
       // Transform the code to regular JS
-      const output = await transform(input);
-      if (!output) {
+      const transformedCode = await transform(sourceCode);
+      if (!transformedCode) {
         throw new Error("Unable to transform code");
       }
-      return output;
+      return transformedCode;
     }
   };
 
@@ -36,25 +52,21 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-async function hideCommand() {
-  const { activeTextEditor } = vscode.window;
-  if (!activeTextEditor) {
-    vscode.window.showErrorMessage("Couldn't find an active editor");
-    return;
-  }
-  const activeEditorTitle = activeTextEditor.document.fileName;
+function findEditorByDocumentName(name: string): vscode.TextEditor | undefined {
+  return vscode.window.visibleTextEditors.find(editor => {
+    return editor.document.fileName === name;
+  });
+}
 
+async function getTransformedDoc(
+  sourceDocument: vscode.TextDocument
+): Promise<vscode.TextDocument> {
   // Get transformed code using our provider
-  const path = `${activeEditorTitle} (transformed)`; // new editor title will equal this
+  // The URI path identifies the source document, but will also be the name
+  // of the transformed document. Hence, we add a suffix to let the user distinguish them
+  const path = `${sourceDocument.fileName}${transformedDocumentSuffix}`;
   const uri = vscode.Uri.parse(`${myScheme}:${path}`, true);
-  let plainDocument;
-  try {
-    plainDocument = await vscode.workspace.openTextDocument(uri);
-  } catch (e) {
-    const message = e.message || `Unknown error: ${e.toString()}`;
-    vscode.window.showErrorMessage(message);
-    return;
-  }
+  const plainDocument = await vscode.workspace.openTextDocument(uri);
 
   // Set language to JS
   const jsDocument = await vscode.languages.setTextDocumentLanguage(
@@ -62,6 +74,28 @@ async function hideCommand() {
     "javascript"
   );
 
-  vscode.window.showTextDocument(jsDocument);
+  return jsDocument;
+}
+
+async function hideTypeScript() {
+  const { activeTextEditor } = vscode.window;
+  if (!activeTextEditor) {
+    vscode.window.showErrorMessage("Couldn't find an active editor");
+    return;
+  }
+
+  const sourceDoc = activeTextEditor.document;
+  watchingDocuments.add(sourceDoc);
+
+  let transformedDoc;
+  try {
+    transformedDoc = await getTransformedDoc(sourceDoc);
+  } catch (e) {
+    const message = e.message || `Unknown error: ${e.toString()}`;
+    vscode.window.showErrorMessage(message);
+    return;
+  }
+
+  vscode.window.showTextDocument(transformedDoc);
   vscode.window.showInformationMessage("Transformed code");
 }
