2cec08906e2333c5d768564119557d78274c5129
Nimo Beeren <nimo@deity.io>
2020-02-05 01:18:25 +0100

    make created editor read-only

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,52 @@
 import * as vscode from "vscode";
 import { transform } from "./transform";
 
-async function transformActiveEditor() {
-  // Get the code from the active editor
-  const { activeTextEditor } = vscode.window;
-  if (!activeTextEditor) {
-    vscode.window.showErrorMessage("Couldn't find an active editor");
-    return;
-  }
-  const source = activeTextEditor.document.getText();
+const myScheme = "hide-typescript";
 
-  // Transform the code to regular JS
-  const transformedSource = await transform(source);
-  if (!transformedSource) {
-    vscode.window.showErrorMessage("Unable to transform code");
-    return;
-  }
-  const transformedDocument = await vscode.workspace.openTextDocument({
-    language: "javascript",
-    content: transformedSource
-  });
-
+function transformActiveEditor() {
   // Show the transformed code
-  vscode.window.showTextDocument(transformedDocument);
-  vscode.window.showInformationMessage("Transformed code");
+  const uri = vscode.Uri.parse(`${myScheme}:active`, true); // the `active` part doesn't matter
+  vscode.window.showTextDocument(uri);
+  vscode.window.showInformationMessage("Transformed code"); // TODO: do something when it fails
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "extension.hideTypeScript",
-    transformActiveEditor
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.hideTypeScript",
+      transformActiveEditor
+    )
   );
 
-  context.subscriptions.push(disposable);
+  const TransformedCodeProvider = class
+    implements vscode.TextDocumentContentProvider {
+    async provideTextDocumentContent(/* uri: vscode.Uri */): Promise<string> {
+      // Get the source code from the active editor
+      const { activeTextEditor } = vscode.window;
+      if (!activeTextEditor) {
+        // TODO: some way to abort without throwing
+        throw new Error("Couldn't find an active editor");
+      }
+      const input = activeTextEditor.document.getText();
+
+      // Transform the code to regular JS
+      const output = await transform(input);
+      if (!output) {
+        // TODO: some way to abort without throwing
+        throw new Error("Unable to transform code");
+      }
+      return output;
+      // TODO: make it understand that the output is JS code
+      // TODO: set document title to something sensible
+    }
+  };
+
+  context.subscriptions.push(
+    vscode.workspace.registerTextDocumentContentProvider(
+      myScheme,
+      new TransformedCodeProvider()
+    )
+  );
 }
 
 export function deactivate() {}
--- src/transform.ts
+++ src/transform.ts
@@ -2,6 +2,7 @@ import { transformAsync as babelTransform } from "@babel/core";
 import pluginTransformTypeScript from "@babel/plugin-transform-typescript";
 
 export async function transform(code: string) {
+  // TODO: make it work with tsx
   const babelResult = await babelTransform(code, {
     plugins: [pluginTransformTypeScript]
   });
