2c0798027b95037e896d939ef0b448f9897916b0
Nimo Beeren <nimo@deity.io>
2020-02-05 12:51:28 +0100

    set editor title to something sensible

--- src/extension.ts
+++ src/extension.ts
@@ -11,11 +11,25 @@ export function activate(context: vscode.ExtensionContext) {
   const TransformedCodeProvider = class
     implements vscode.TextDocumentContentProvider {
     async provideTextDocumentContent(/* uri: vscode.Uri */): Promise<string> {
-      return transformActiveEditor();
-      // TODO: set document title to something sensible
+      // Get the source code from the active editor
+      const { activeTextEditor } = vscode.window;
+      if (!activeTextEditor) {
+        // TODO: some way to abort without throwing
+        throw new Error("Couldn't find an active editor");
+      }
+      const input = activeTextEditor.document.getText();
+
+      // Transform the code to regular JS
+      const output = await transform(input);
+      if (!output) {
+        // TODO: some way to abort without throwing
+        throw new Error("Unable to transform code");
+      }
+      return output;
     }
   };
 
+  // Register our provider to handle URIs with our scheme
   context.subscriptions.push(
     vscode.workspace.registerTextDocumentContentProvider(
       myScheme,
@@ -25,31 +39,20 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function hideCommand() {
-  // Show the transformed code
-  const uri = vscode.Uri.parse(`${myScheme}:active`, true); // the `active` part doesn't matter
+  const { activeTextEditor } = vscode.window;
+  const activeEditorTitle = activeTextEditor?.document.fileName;
+
+  // Get transformed code using our provider
+  const path = `${activeEditorTitle} (transformed)`; // new editor title will equal this
+  const uri = vscode.Uri.parse(`${myScheme}:${path}`, true);
   const plainDocument = await vscode.workspace.openTextDocument(uri);
+
+  // Set language to JS
   const jsDocument = await vscode.languages.setTextDocumentLanguage(
     plainDocument,
     "javascript"
   );
+
   vscode.window.showTextDocument(jsDocument);
   vscode.window.showInformationMessage("Transformed code"); // TODO: do something when it fails
-}
-
-async function transformActiveEditor(): Promise<string> {
-  // Get the source code from the active editor
-  const { activeTextEditor } = vscode.window;
-  if (!activeTextEditor) {
-    // TODO: some way to abort without throwing
-    throw new Error("Couldn't find an active editor");
-  }
-  const input = activeTextEditor.document.getText();
-
-  // Transform the code to regular JS
-  const output = await transform(input);
-  if (!output) {
-    // TODO: some way to abort without throwing
-    throw new Error("Unable to transform code");
-  }
-  return output;
 }
